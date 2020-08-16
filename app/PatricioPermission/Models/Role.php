c960fde0d25dac3496c6d01134674aa76e64bf6e
patorma <pocontreras@ing.ucsc.cl>
2020-08-16 01:27:13 -0400

    se configuro para asociar en relacion muchos a muchos de rol y usuario agregar registro una vez

--- app/PatricioPermission/Models/Role.php
+++ app/PatricioPermission/Models/Role.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    //es: desde aqui
+    //en: from here
+    // esto es porque enviaremos datos en forma masiva
+    protected $fillable = [
+        'name', 'slug', 'description', 'full-access',
+    ];
+
+    // que cuando se seleccione un rol y se llame a esta funcion
+    // esto traera a todos los usuarios que esten relacionados con este rol
+    public function users(){
+        //belongsToMany() relacion muchos a muchos 
+        //se indica con withTimesTamps() que en esta tabla intermedia
+        //actualizxamos fechas
+        return $this->belongsToMany('App\User')->withTimesTamps();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Route;
+use App\PatricioPermission\Models\Role;
+//use App\PatricioPermission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,40 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function () {
+
+    /*
+        return   Role::create([
+        'name' => 'Admin',
+        'slug' => 'admin',
+        'description' => 'Administrator',
+        'full-access' => 'yes'
+    ]);
+
+    return   Role::create([
+        'name' => 'Guest',
+        'slug' => 'guest',
+        'description' => 'guest',
+        'full-access' => 'no'
+    ]);
+
+    return   Role::create([
+        'name' => 'test',
+        'slug' => 'test',
+        'description' => 'test',
+        'full-access' => 'no'
+    ]);
+    */
+  
+   $user = User::find(1);
+   //con attach se crea registros
+   //$user->roles()->attach([1,2,3]);
+   // con esto eliminamos el rol 1 asociado al usuario 1
+   //$user->roles()->detach([3]);
+   //esto hace el attach y detach al mismo tiempo
+   $user->roles()->sync([1,2]);
+   return  $user->roles;
+
+   
+});
